8c61ae1099c91f59d7ce8d859a3cd33083f9aae1
Martin Kemppi <dusperin1234@gmail.com>
2024-05-06 11:14:52 +0300

    hiirekursori hõljumisel lennuk õhus, väärtus on null ja maks. väärtus muudab tumepunaseks

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
-$kasutaja = 'd123172_timur';
-$serverinimi = 'd123172.mysql.zonevs.eu';
-$parool='56360881Tt.';
-$andmebaas='d123172_andmebaas';
-$yhendus=new mysqli($serverinimi,$kasutaja,$parool,$andmebaas);
+$kasutaja='yaroslavyekasov';
+$serverinimi='localhost';
+$parool='123456';
+$andmebaas='lennujaam';
+$yhendus=new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
 $yhendus->set_charset('UTF8');
